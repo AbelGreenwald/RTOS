07dc02419ae3fc8cf19e2cbd23622d9c86144e42
AbelGreenwald <abel@localhost.localdomain>
2018-02-04 10:51:37 -0600

    mutex example with led's

--- main.cpp
+++ main.cpp
@@ -10,30 +10,36 @@ DigitalOut led3(PB_14);
 int main() {
     osKernelStart();
     osMutexCreate(osMutex(sem1));
-    osThreadCreate(osThread(t1), NULL);
     osThreadCreate(osThread(t2), NULL);
+    osThreadCreate(osThread(t1), NULL);
     osThreadCreate(osThread(t3), NULL);
+
     osDelay(osWaitForever);
 }
 
 void t1(void const *argument){
-    osMutexWait(sem1, 0);
-    osDelay(20);
-    led1 = !led1;
-    osMutexRelease(sem1);
+    while (true) {
+        osMutexWait(sem1, 0);
+        osDelay(80);
+        led1 = !led1;
+        osMutexRelease(sem1);
+    }
 }
 
 void t2(void const *argument){
-
-    osMutexWait(sem1, 0);
-    osDelay(20);
-    led2 = !led2;
-    osMutexRelease(sem1);
+    while (true) {
+        osMutexWait(sem1, 0);
+        osDelay(40);
+        led2 = !led2;
+        osMutexRelease(sem1);
+    }
 }
 
 void t3(void const *argument){
-    osMutexWait(sem1, 0);
-    osDelay(20);
-    led2 = !led2;
-    osMutexRelease(sem1);
+    while (true) {
+        osMutexWait(sem1, 0);
+        osDelay(20);
+        led3 = !led3;
+        osMutexRelease(sem1);
+    }
 }
